9aa6b246f51ef8eab145a3962ccc3fde838d9633
Carter_Scherer <carterjohnscherer@gmail.com>
2024-11-04 19:51:46 -0800

    register styles

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,6 +4,7 @@ import { useAuth } from '../context/authContext'
 import { doCreateUserWithEmailAndPassword } from '../firebase/auth'
 import { db } from '../firebase/firebase' // Import Firestore instance
 import { doc, setDoc } from 'firebase/firestore' // Firestore methods
+import "../scss/components/_register.scss"
 
 const Register = () => {
     const navigate = useNavigate()
@@ -50,81 +51,89 @@ const Register = () => {
     }
 
     return (
-        <>
+        <div className="register-page">
             {userLoggedIn && (<Navigate to={'/home'} replace={true} />)}
-
-            <main>
-                <div>
-                    <div>
+            
+            <main className="register-container">
+                <div className="register-box">
+                    <header className="register-header">
                         <h3>Create a New Account</h3>
-                    </div>
-
-                    <form onSubmit={onSubmit}>
+                    </header>
+    
+                    <form className="register-form" onSubmit={onSubmit}>
                         {/* First Name Field */}
-                        <div>
-                            <label>First Name</label>
+                        <div className="form-group">
+                            <label htmlFor="firstName">First Name</label>
                             <input
+                                id="firstName"
                                 type="text"
                                 required
                                 value={firstName}
                                 onChange={(e) => setFirstName(e.target.value)}
+                                className="form-input"
                             />
                         </div>
-
+    
                         {/* Email Field */}
-                        <div>
-                            <label>Email</label>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
                             <input
+                                id="email"
                                 type="email"
                                 autoComplete='email'
                                 required
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
+                                className="form-input"
                             />
                         </div>
-
+    
                         {/* Password Field */}
-                        <div>
-                            <label>Password</label>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
                             <input
+                                id="password"
                                 disabled={isRegistering}
                                 type="password"
                                 autoComplete='new-password'
                                 required
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
+                                className="form-input"
                             />
                         </div>
-
+    
                         {/* Confirm Password Field */}
-                        <div>
-                            <label>Confirm Password</label>
+                        <div className="form-group">
+                            <label htmlFor="confirmPassword">Confirm Password</label>
                             <input
+                                id="confirmPassword"
                                 disabled={isRegistering}
                                 type="password"
                                 autoComplete='off'
                                 required
                                 value={confirmPassword}
                                 onChange={(e) => setConfirmPassword(e.target.value)}
+                                className="form-input"
                             />
                         </div>
-
+    
                         {errorMessage && (
-                            <span>{errorMessage}</span>
+                            <span className="error-message">{errorMessage}</span>
                         )}
-
-                        <button type="submit" disabled={isRegistering}>
+    
+                        <button className="submit-button" type="submit" disabled={isRegistering}>
                             {isRegistering ? 'Signing Up...' : 'Sign Up'}
                         </button>
-
-                        <div>
-                            Already have an account? {' '}
+    
+                        <div className="login-link">
+                            Already have an account?{' '}
                             <Link to={'/login'}>Continue</Link>
                         </div>
                     </form>
                 </div>
             </main>
-        </>
+        </div>
     )
 }
 
